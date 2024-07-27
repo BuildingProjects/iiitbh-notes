3ca600bb3409e4936cd3b6041cc4c74ff46e69e7
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2024-07-27 16:39:42 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import CodingSlide from './components/CodingSlide'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>Hello</div>
+    <>
+    <Header/>
+    <CodingSlide/>
+    </>
   )
 }
 
