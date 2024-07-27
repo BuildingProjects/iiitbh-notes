18394b9cbc4473b0b6d10ece0418ddd8658c79df
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2024-07-27 17:17:05 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import './App.css'
-<<<<<<< HEAD
 import Hero from './components/Hero'
-=======
 import Header from './components/Header'
 import CodingSlide from './components/CodingSlide'
->>>>>>> 3ca600bb3409e4936cd3b6041cc4c74ff46e69e7
 
 function App() {
 
   return (
-<<<<<<< HEAD
-    <Hero/>
-=======
     <>
     <Header/>
+    <Hero/>
     <CodingSlide/>
     </>
->>>>>>> 3ca600bb3409e4936cd3b6041cc4c74ff46e69e7
   )
 }
 
