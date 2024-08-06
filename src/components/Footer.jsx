a345ae6a91f63ec88721ec6827546fe15bc45825
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2024-08-06 16:35:31 +0530

    added links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,16 +46,16 @@ function Footer() {
                 <ul className="text-white font-medium">
                   <li className="mb-4">
                     <a
-                      href="https://github.com/BuildingProjects/iiitbh-notes"
+                      href="https://www.instagram.com/iiitbh_notes/"
                       className="hover:underline"
                       target="_blank"
                     >
-                      GitHub
+                      Instagram
                     </a>
                   </li>
                   <li>
                     <a
-                      href="https://www.linkedin.com/in/roshan2003/"
+                      href="https://t.me/+8EGaWV-IdekwYjk9"
                       className="hover:underline"
                       target="_blank"
                     >
@@ -87,7 +87,7 @@ function Footer() {
             </div>
           </div>
           <hr className="my-6 border-white sm:mx-auto lg:my-8" />
-          <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="sm:flex sm:items-center  sm:justify-between ">
             <span className="text-2xl text-white sm:text-center">
               ©{currentYear}{" "}
               <a href="/" className="hover:underline">
@@ -95,36 +95,34 @@ function Footer() {
               </a>
               . All Rights Reserved.
             </span>
-            <div className="flex mt-4 sm:justify-center sm:mt-0">
-              <a href="#" className="text-white hover:text-gray-900  ms-5">
-                <svg
-                  className='w-6 h-6'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='currentColor'
-                  viewBox='0 0 21 16'
-                >
-                  <path d='M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z' />
-                </svg>
-                <span className="sr-only">Instagram</span>
-              </a>
-              <a href="#" className="text-white hover:text-gray-900  ms-5">
+            <div className="flex mt-4 sm:justify-center sm:mt-0 ">
+              <a
+                href="https://www.instagram.com/iiitbh_notes/"
+                className="text-white hover:text-gray-900  ms-5"
+                target="_blank"
+              >
                 <svg
                   className="w-6 h-6"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
-                  viewBox="0 0 20 17"
+                  viewBox="0 0 24 24"
                 >
                   <path
                     fill-rule="evenodd"
-                    d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                    d="M12 2.162c3.26 0 3.63.012 4.91.071a6.942 6.942 0 0 1 2.2.46 4.293 4.293 0 0 1 2.804 2.804 6.94 6.94 0 0 1 .46 2.2c.059 1.28.071 1.65.071 4.91s-.012 3.63-.071 4.91a6.94 6.94 0 0 1-.46 2.2 4.293 4.293 0 0 1-2.804 2.804 6.942 6.942 0 0 1-2.2.46c-1.28.059-1.65.071-4.91.071s-3.63-.012-4.91-.071a6.942 6.942 0 0 1-2.2-.46 4.293 4.293 0 0 1-2.804-2.804 6.94 6.94 0 0 1-.46-2.2C2.174 15.01 2.162 14.64 2.162 12s.012-3.63.071-4.91a6.94 6.94 0 0 1 .46-2.2A4.293 4.293 0 0 1 5.237 4.75a6.942 6.942 0 0 1 2.2-.46c1.28-.059 1.65-.071 4.91-.071zM12 0C8.741 0 8.33.012 7.06.07a9.934 9.934 0 0 0-3.114.655 6.716 6.716 0 0 0-4.356 4.356A9.934 9.934 0 0 0 0 7.06C.012 8.33 0 8.741 0 12s.012 3.63.071 4.91a9.934 9.934 0 0 0 .655 3.114 6.716 6.716 0 0 0 4.356 4.356 9.934 9.934 0 0 0 3.114.655C8.33 23.988 8.741 24 12 24s3.63-.012 4.91-.071a9.934 9.934 0 0 0 3.114-.655 6.716 6.716 0 0 0 4.356-4.356 9.934 9.934 0 0 0 .655-3.114C24.012 15.63 24 15.22 24 12s-.012-3.63-.071-4.91a9.934 9.934 0 0 0-.655-3.114 6.716 6.716 0 0 0-4.356-4.356A9.934 9.934 0 0 0 16.91.07C15.63.012 15.22 0 12 0zM12 5.84a6.16 6.16 0 1 0 0 12.32A6.16 6.16 0 0 0 12 5.84zm0 10.06a3.9 3.9 0 1 1 0-7.8 3.9 3.9 0 0 1 0 7.8zm4.48-8.56a1.308 1.308 0 1 1-2.617 0 1.308 1.308 0 0 1 2.617 0z"
                     clip-rule="evenodd"
                   />
                 </svg>
-                <span className="sr-only">Telegram</span>
+
+                <span className="sr-only">Instagram</span>
               </a>
-              <a href="#" className="text-white hover:text-gray-900 ms-5">
+              
+              <a
+                href="https://github.com/BuildingProjects/iiitbh-notes"
+                className="text-white hover:text-gray-900 ms-5"
+                target="_blank"
+              >
                 <svg
                   className="w-6 h-6"
                   aria-hidden="true"
